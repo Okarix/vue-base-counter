7c3dcd9f0d8886ff830df2e339543073160df2f8
Adiar <93129221+Okarix@users.noreply.github.com>
2024-04-10 23:09:14 +0500

    options api example

---
+++ vue-counter/src/components/Counter.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+	data() {
+		return {
+			count: 0,
+		};
+	},
+	methods: {
+		increment() {
+			this.count++;
+		},
+	},
+};
+</script>
+
+<template>
+	<div>
+		<h1>{{ count }}</h1>
+		<button @click="increment">+</button>
+	</div>
+</template>
