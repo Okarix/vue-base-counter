5e7eea3d8cce79aea2867e68d0918a7420b4f920
Adiar <93129221+Okarix@users.noreply.github.com>
2024-04-10 23:17:01 +0500

    composition api example

--- vue-counter/src/components/Counter.vue
+++ vue-counter/src/components/Counter.vue
@@ -1,16 +1,11 @@
-<script>
-export default {
-	data() {
-		return {
-			count: 0,
-		};
-	},
-	methods: {
-		increment() {
-			this.count++;
-		},
-	},
-};
+<script setup>
+import { ref } from 'vue';
+
+const count = ref(0);
+
+function increment() {
+	count.value++;
+}
 </script>
 
 <template>
