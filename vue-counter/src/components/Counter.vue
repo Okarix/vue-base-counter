c404441fe10c481c804681fafb938e4115ba4419
Adiar <93129221+Okarix@users.noreply.github.com>
2024-04-10 23:22:43 +0500

    v-model

--- vue-counter/src/components/Counter.vue
+++ vue-counter/src/components/Counter.vue
@@ -6,11 +6,17 @@ const count = ref(0);
 function increment() {
 	count.value++;
 }
+
+// function updateCount(event) {
+// 	count.value = Number(event.target.value);
+// }
 </script>
 
 <template>
 	<div>
 		<h1>{{ count }}</h1>
 		<button @click="increment">+</button>
+		<!-- <input @input="updateCount" /> -->
+		<input v-model="count" />
 	</div>
 </template>
